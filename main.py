7a7ec436dd70b1a3efaaa32577d56078d4673059
DJuuDi <152791461+DJuuDi@users.noreply.github.com>
2024-02-11 17:30:25 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QMainWindow
+from ui import Ui_MainWindow
+
+class Widget(QMainWindow):
+    def   __init__(self):
+        super().__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.noets = {
+            "Перша замітка":{
+                "текст": "Це текст першої замітки",
+                "теги": []
+            }
+        }
+app = QApplication([])
+ex = Widget()
+ex.show()
+app.exec_()
