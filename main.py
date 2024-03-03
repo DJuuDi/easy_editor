79fd1a6a0a58cdddeb9d2f4b8b82d7eeb87d71af
DJuuDi <152791461+DJuuDi@users.noreply.github.com>
2024-03-03 17:30:55 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -14,13 +14,21 @@ class Widget(QMainWindow):
         self.ui.save_btn.clicked.connect(self.save_note)
         self.ui.create_btn.clicked.connect(self.create_note)
         self.ui.delete_btn.clicked.connect(self.delete_note)
+        self.ui.add_teg_btn.clicked.connect(self.add_tag)
+        self.ui.unpin_teg_btn.clicked.connect(self.del_tag)
 
     def show_note(self):
         self.name = self.ui.noet_list.selectedItems()[0].text()
         self.ui.title_edit.setText(self.name)
         self.ui.text_edit.setText(self.notes[self.name]["текст"])
+        self.ui.noet_lnst.clear()
+        self.ui.noet_lnst.addItems(self.notes[self.name]["теги"])
 
+        
     def save_note(self):
+        tags = []
+        for i in range(self.ui.tag_list.count()):
+            tags.append(self.ui.tag_list.item(i).text())
         self.notes[self.ui.title_edit.text()] = {
             "текст": self.ui.text_edit.toPlainText(),
             "теги":[]
@@ -43,8 +51,8 @@ class Widget(QMainWindow):
                 self.notes = json.load(file)
         except:
             self.notes = {
-                "Перша замітка":{
-                    "текст": "Це текст першої замітки",
+                "Гугл замітка":{
+                    "текст": "акк: juliagergun@gmail.com, пароль: 5345julia5345",
                     "теги": []
             }
         }
@@ -59,8 +67,21 @@ class Widget(QMainWindow):
         except:
             print("")
 
+    def add_tag(self):
+        tag_name = self.ui.search_l.text()
+        if tag_name!="":
+            if tag_name not in self.notes[self.name]["теги"]:
+                self.notes[self.name]["теги"].append(tag_name)
+                self.ui.noet_lnst.clear()
+                self.ui.noet_lnst.addItems(self.notes[self.name]["теги"])
 
-
+    def del_tag(self):
+        if self.ui.noet_lnst.selectedItems():
+            tag_name = self.ui.noet_lnst.selectedItems()[0].text()
+            if tag_name in self.notes[self.name]["теги"]:
+                    self.notes[self.name]["теги"].remove(tag_name)
+                    self.ui.noet_lnst.clear()
+                    self.ui.noet_lnst.addItems(self.notes[self.name]["теги"])
 app = QApplication([])
 ex = Widget()
 ex.show()
